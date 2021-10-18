b554617d90c82d84672cd825b7cf65bf4aebcf58
Giuseppe Moi <giuseppemoi@Giuseppes-MacBook-Air.local>
2021-10-18 13:57:54 +0200

    search page

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -1,5 +1,107 @@
-import React from "react";
+import Nav from "./NavBar"
+import {BiSearch} from "react-icons/bi"
+import {IconContext} from "react-icons"
+import {useEffect, useState} from "react";
+import axios from "axios";
 
-export default function Discover() {
-    return <h2>Discover</h2>;
+export default function Discover(props) {
+    const genresArrays = props.genres
+
+    const API_KEY = 'api_key=61330235259d30ec7f0c37b884800e04'
+    const BASE_URL = 'https://api.themoviedb.org/3/'
+    const URL_Discover = 'discover/movie?'
+
+    const [genreId, setGenreId] = useState(28)
+    const [page, setPage] = useState(1)
+
+    const PARAMS = `&language=en-US&sort_by=popularity.desc&page=${page}&with_genres=`
+
+    async function moviesDataByGenrePromise(id) {
+        return await axios.get(BASE_URL + URL_Discover + API_KEY + PARAMS + id)
+    }
+
+    const [listMoviesGenre, setListMoviesGenre] = useState([])
+
+    async function fetchMovie() {
+        const moviesData = await moviesDataByGenrePromise(genreId)
+        setListMoviesGenre(moviesData.data.results)
+    }
+
+    useEffect(() => {
+        fetchMovie()
+    }, [genreId])
+
+    useEffect(() => {
+        document.addEventListener('scroll', (e) => {
+            console.log(e.path)
+            if ((e.path[1].innerHeight + e.path[1].scrollY) >= e.path[0].body.offsetHeight) {
+                setPage(page + 1)
+                moviesDataByGenrePromise(genreId).then((result) => {
+                    // setListMoviesGenre([...listMoviesGenre, ...result.data.results])
+                    const newData = result.data.results
+                    setListMoviesGenre(listMoviesGenre.concat(newData))
+                })
+            }
+        })
+    }, [listMoviesGenre])
+
+    function MoviesGenres() {
+        return (
+            listMoviesGenre.map(movie => {
+                return (
+                    <div className="movieGenre">
+                        <div className="divSize">
+                            <img src={"https://image.tmdb.org/t/p/w400" + movie.poster_path} alt={movie.title}/>
+                        </div>
+                        <p>{movie.title} <span>({movie.release_date.split('-')[0]})</span></p>
+                    </div>
+                )
+            })
+        )
+    }
+
+    // const [tagStyle, setTagStyle] = useState(null)
+
+    function changeGenre(e) {
+        e.preventDefault()
+        setGenreId(e.target.id)
+        e.target.style.color = '#FF8F71'
+        e.target.style.textDecoration = 'underline'
+        // setTagStyle((prev) => prev = document.getElementById(e.target.id).style)
+        // setTagStyle((prev) => prev.color = '#FF8F71')
+        // setTagStyle(tagStyle.textDecoration = 'underline')
+        // console.log(tagStyle)
+        // tagStyle.color = '#FF8F71'
+        // tagStyle.textDecoration = 'underline'
+    }
+
+    function NavGenres() {
+        return (
+            genresArrays.map(genre => {
+                return (
+                    // <input onClick={(e) => changeGenre(e)} id={genre.id} type="button" value={genre.name}/>
+                    // <a onClick={(e) => changeGenre(e)} id={genre.id} >{genre.name}</a>
+                    <p onClick={(e) => changeGenre(e)} id={genre.id} >{genre.name}</p>
+                )
+            })
+        )
+    }
+    return (
+        <div>
+            <Nav/>
+            <h1 className='title'><span>Movie</span>Browser</h1>
+            <div className='input'>
+                <input />
+                <IconContext.Provider value={{size: '3vh'}}>
+                    <BiSearch />
+                </IconContext.Provider>
+            </div>
+            <nav className='navGenres'>
+                <NavGenres/>
+            </nav>
+            <div className='moviesGenre'>
+                <MoviesGenres/>
+            </div>
+        </div>
+    )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,54 @@
+import Nav from "./NavBar"
+import {AiFillPlayCircle} from "react-icons/ai"
+import {IconContext} from "react-icons"
+import Carousel from 'react-elastic-carousel'
+
+
 export default function Home(props) {
 
     function RandomMovie() {
-        const dataMovie = props.movies[Math.floor(Math.random() * 20)]
+        const movie = props.movies[Math.floor(Math.random() * 20)]
         return (
             <div className="images">
-                <img src={dataMovie && "https://image.tmdb.org/t/p/w300" + dataMovie.poster_path}
-                     alt={dataMovie && dataMovie.title}/>
+                <img src={movie && "https://image.tmdb.org/t/p/w500" + movie.backdrop_path}
+                     alt={movie && movie.title}/>
+                <div className="spotlight">
+                    <IconContext.Provider value={{size: '7vw'}}>
+                        <AiFillPlayCircle />
+                    </IconContext.Provider>
+                    <div>
+                        <p>Movie Spotlight</p>
+                        <h3>{movie && movie.title}</h3>
+                    </div>
+                </div>
             </div>
         )
     }
 
+    function Slider() {
+        const dataMovies = props.movies
+
+        return  dataMovies.map(movie => {
+            return <img src={"https://image.tmdb.org/t/p/w400" + movie.poster_path} alt={movie.title}/>
+        })
+    }
+
     return (
         <div>
+            <Nav/>
             <h1 className='title'><span>Movie</span>Browser</h1>
-            {RandomMovie()}
+            <RandomMovie />
             <h2 className='title'>Trending</h2>
-            <div>carousel</div>
+            <div className="slider">
+                <Carousel
+                    preventDefaultTouchmoveEvent={true}
+                    children={Slider()}
+                    pagination={false}
+                    showArrows={false}
+                    enableAutoPlay={true}
+                    autoPlaySpeed={3000}
+                />
+            </div>
         </div>
     )
 }
