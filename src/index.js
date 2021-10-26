0a984db553103d2f934daa198ecb9db080faeace
Giuseppe Moi <giuseppemoi@Giuseppes-MacBook-Air.local>
2021-10-26 11:27:22 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react'
 import {render} from 'react-dom'
 
 import App from './components/App'
-import './assets/style.scss'
+import './assets/style.css'
 
 render(<App/>, document.getElementById('app'))
